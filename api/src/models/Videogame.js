490304f2b099a952abd332ddefd1f88cc9af8a8f
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-09-24 16:27:25 -0300

    models and associations

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,11 +1,43 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
+
+const dateRE =
+  /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define("videogame", {
+    id: {
+      //Genera un ID con este formato: a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    releaseDate: {
+      type: DataTypes.STRING,
+      isDate(value) {
+        value.search(dateRE);
+      },
+    },
+    rating: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        isNumeric: true,
+        min: 0,
+        max: 5,
+      },
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: false,
     },
   });
