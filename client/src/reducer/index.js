e20add9249b898fb9952df0b6c208413e9a91e0c
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-09-30 19:42:01 -0300

    GET_VIDEOGAMES and SEARCH_VIDEOGAMES

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,10 +1,23 @@
 const initialState = {
   videogames: [],
   detail: {},
+  videogamesSearch: [],
 };
 
 export default function rootReducer(state = initialState, action) {
   switch (action.type) {
+    case "GET_VIDEOGAMES":
+      return {
+        //Devuelve una copia del state y le concatena en la propiedad videogames todos los videojuegos
+        ...state,
+        videogames: action.payload,
+      };
+    case "SEARCH_VIDEOGAME":
+      return {
+        //Devuelve una copia del state y le concatena en la propiedad videogamesSearch los videojuegos que traiga de la busqueda
+        ...state,
+        videogamesSearch: action.payload,
+      };
     default:
       return { ...state };
   }
