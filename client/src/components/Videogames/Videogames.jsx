b40c422fbc865c58ceaf02f6671aceaf7a17439d
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-09-30 19:44:11 -0300

    Videogames component created

---
+++ client/src/components/Videogames/Videogames.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import s from './Videogames.module.css'
+
+export default function Videogames(){
+    return(
+        <div>
+            {/* Despachar action getVideogames */}
+            <h1>Videogames</h1>
+        </div>
+    )
+}
