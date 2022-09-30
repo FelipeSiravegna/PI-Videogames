3bb7e939ea7833ed857be5e5c8af9001e5cf6b6a
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-09-30 17:32:25 -0300

    handleClick button landingPage

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import s from './LandingPage.module.css';
 
 export default function LandingPage(){
+    const history = useHistory();
+    const handleClick = () => {
+        history.push('/videogames');
+    }
+    
+
     return(
         <div className={s.background}>
             <div className={s.container}>
                 <h1 className={s.text}>WELCOME</h1>
                 <h2 className={s.text}>TO THE VIDEOGAME APP</h2>
                 <div className={s.button}>
-                    <button className={s.letsPlay}>LETS PLAY!</button>
+                    <button className={s.letsPlay} onClick={handleClick}>
+                        LETS PLAY!
+                    </button>
                 </div>
             </div>
         </div>
