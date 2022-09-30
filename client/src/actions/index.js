59add75b47a312a7e0a335248bfdf0dd8f33063b
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-09-30 19:42:57 -0300

    getVideogames and searchVideogame actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,28 @@
+//Traigo todos los videojuegos
+export function getVideogames() {
+  return function (dispatch) {
+    return fetch(`http://localhost:3001/videogames`)
+      .then((response) => response.json())
+      .then((responsejson) => {
+        dispatch({
+          type: "GET_VIDEOGAMES",
+          payload: responsejson,
+        });
+      });
+  };
+}
+
+//Busco un videojuego usando el searchBar
+export function searchVideogame(videogameName) {
+  console.log("Buscando videojuegos");
+  return function (dispatch) {
+    return fetch(`http://localhost:3001/videogames?name=${videogameName}`)
+      .then((response) => response.json())
+      .then((responsejson) => {
+        dispatch({
+          type: "SEARCH_VIDEOGAME",
+          payload: responsejson,
+        });
+      });
+  };
+}
