d9433601a979f87cb4a52954fa5fd366352d7f0f
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-10-05 20:04:25 -0300

    card component created

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 // import s from './Card.module.css'
 
-export default function Card(){
+export default function Card({name, image, genres}){
     return(
         <div>
-            <h1>Card</h1>
+            <h3>{name}</h3>
+            <img src={image} alt='game' width='150px' height='200px'/>
+            <h5>{genres}</h5>
         </div>
     )
 }
