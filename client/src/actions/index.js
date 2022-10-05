b82a6cac6369e15299fb2d095ef1a0590faea8bb
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-10-05 20:07:08 -0300

    getVideogames and getGenres action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,27 +1,21 @@
-//Traigo todos los videojuegos
 export function getVideogames() {
-  return function (dispatch) {
-    return fetch(`http://localhost:3001/videogames`)
+  return (dispatch) => {
+    fetch(`http://localhost:3001/videogames`)
       .then((response) => response.json())
-      .then((responsejson) => {
-        return dispatch({
-          type: "GET_VIDEOGAMES",
-          payload: responsejson,
-        });
+      .then((json) => {
+        dispatch({ type: "GET_VIDEOGAMES", payload: json });
       });
   };
 }
 
-//Busco un videojuego usando el searchBar
-export function searchVideogame(videogameName) {
-  // console.log("Buscando videojuegos");
-  return function (dispatch) {
-    return fetch(`http://localhost:3001/videogames?name=${videogameName}`)
+export function getGenres() {
+  return (dispatch) => {
+    fetch(`http://localhost:3001/genres`)
       .then((response) => response.json())
-      .then((responsejson) => {
-        return dispatch({
-          type: "SEARCH_VIDEOGAME",
-          payload: responsejson,
+      .then((json) => {
+        dispatch({
+          type: "GET_GENRES",
+          payload: json,
         });
       });
   };
