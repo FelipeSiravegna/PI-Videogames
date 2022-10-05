4a38155ea9b3abedbb978bc679c5b53d2d004e27
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-10-05 17:02:01 -0300

    genres doesn't call the api each time, only when server starts

--- api/index.js
+++ api/index.js
@@ -19,16 +19,33 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const axios = require("axios");
+const { Genre } = require("./src/db");
+const { API_KEY } = process.env;
 
-// // Syncing all the models at once.
-// conn.sync({ force: true }).then(() => {
-//   server.listen(3001, () => {
-//     console.log("%s listening at 3001"); // eslint-disable-line no-console
-//   });
-// });
+const checkDB = async () => {
+  //Traigo todos los genres de la API
+  const genresAPI = await axios.get(
+    `https://api.rawg.io/api/genres?key=${API_KEY}`
+  );
 
-conn.sync().then(() => {
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  //Por cada genre me hago un findorcreate
+  genresAPI.data.results.forEach((g) => {
+    Genre.findOrCreate({
+      where: { name: g.name },
+    });
+  });
+};
+
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(3001, async () => {
+    console.log("%s listening at 3001", await checkDB()); // eslint-disable-line no-console
   });
 });
+
+// conn.sync().then(() => {
+//   server.listen(3001, async () => {
+//     console.log("%s listening at 3001", await checkDB()); // eslint-disable-line no-console
+//   });
+// });
--- api/src/routes/genres.js
+++ api/src/routes/genres.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
 const { Genre } = require("../db");
-const { API_KEY } = process.env;
 
 router.get("/", async (req, res) => {
   try {
-    //Traigo todos los genres de la API
-    const genresAPI = await axios.get(
-      `https://api.rawg.io/api/genres?key=${API_KEY}`
-    );
-
-    //Por cada genre me hago un findorcreate
-    genresAPI.data.results.forEach((g) => {
-      Genre.findOrCreate({
-        where: { name: g.name },
-      });
-    });
-
     //Los traigo a todos de la db
     const genresDB = await Genre.findAll();
 
