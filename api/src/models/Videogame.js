f98a8405a9a1554b7d16d14bb1cc3b47856f9e41
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-10-10 18:24:56 -0300

    regex removed from models

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,7 +1,5 @@
 const { DataTypes } = require("sequelize");
 
-const dateRE =
-  /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
@@ -33,9 +31,6 @@ module.exports = (sequelize) => {
     },
     releaseDate: {
       type: DataTypes.STRING,
-      isDate(value) {
-        value.search(dateRE);
-      },
     },
     rating: {
       type: DataTypes.DECIMAL,
