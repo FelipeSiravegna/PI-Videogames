b33dc30220a32d4f7c0188379f5ae32e7ee685b9
FelipeSiravegna <felipesiravegna1@gmail.com>
2022-10-13 18:16:50 -0300

    comment

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -14,6 +14,7 @@ export default function Detail(props){
 
     const videogame = useSelector(state => state.detail)
 
+    //Component will unmount
     useEffect(() => {
         return() => {
             dispatch(resetDetail())
