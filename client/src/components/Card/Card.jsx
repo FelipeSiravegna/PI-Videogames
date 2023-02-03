139670fbbcafe2d35333a720c1c2f941296d609a
FelipeSiravegna <felipesiravegna1@gmail.com>
2023-02-03 18:48:51 -0300

    Star emoji added to ranking

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import s from './Card.module.css'
 
-export default function Card({name, image, genres, rating, id}){
-    return(
+export default function Card({ name, image, genres, rating, id }) {
+    return (
         <div className={s.cardDiv}>
             <NavLink className={s.cardH3} to={`/videogame/${id}`}>
                 <h3 className={s.bolderFont}>{name}</h3>
             </NavLink>
-            <img src={image} alt='game'/>
-            <h4>{rating}</h4>
+            <img src={image} alt='game' />
+            <h4>⭐ {rating} ⭐</h4>
             <h5>{genres}</h5>
         </div>
     )
